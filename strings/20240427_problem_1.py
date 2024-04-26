38b37bd7f1ec266cfaba15106dfc0bf7051f528c
Shashank Shekhar <shashankshekhar8534@gmail.com>
2024-04-27 00:00:00 +0530

    Solved strings problem 1 on 2024-04-27

---
+++ strings/20240427_problem_1.py
@@ -0,0 +1,12 @@
+
+"""
+Problem: Dummy Problem Description
+Approach: Brief logic...
+"""
+
+def solution():
+    # Your logic here
+    return "Solved"
+
+if __name__ == "__main__":
+    print(solution())
